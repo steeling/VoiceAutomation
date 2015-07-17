b84331909c867a60b3912987c8b1e1a6b635a3ac
steeling <teeling.sean@gmail.com>
2015-07-16 19:07:58 -0700

    simple looped listen

--- listener.py
+++ listener.py
@@ -6,36 +6,46 @@ class Listener():
   def __init__(self):
     print "Starting up"
   def listen(self):
-    with sr.Microphone() as source:                # use the default microphone as the audio source
-      # r.adjust_for_ambient_noise(source)         # listen for 1 second to calibrate the energy threshold for ambient noise levels
+    while(True):
+      print "Listening"
+      with sr.Microphone() as source:                # use the default microphone as the audio source
+        # r.adjust_for_ambient_noise(source)         # listen for 1 second to calibrate the energy threshold for ambient noise levels
 
-      audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
+        audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
 
-    try:
-      print(r.recognize(audio))                  # recognize speech using Google Speech Recognition
-    except LookupError:                            # speech is unintelligible
-      print("Could not understand audio")
+      try:
+        print(r.recognize(audio))                  # recognize speech using Google Speech Recognition
+      except LookupError:                            # speech is unintelligible
+        print("Could not understand audio")
 
-  def callback(self, recognizer, audio):                          # this is called from the background thread
-    try:
-      print "Looking up: "
-      print(recognizer.recognize(audio))  # received audio data, now need to recognize it
-    except LookupError:
-      print("Oops! Didn't catch that")
-    finally:
-      print "stopping"
+  # def callback(self, recognizer, audio):                          # this is called from the background thread
+  #   try:
+  #     print "Looking up: "
+  #     print(recognizer.recognize(audio))  # received audio data, now need to recognize it
+  #   except LookupError:
+  #     print("Oops! Didn't catch that")
+  #   finally:
+  #     print "stopping"
 
-  def listen2(self):
-    r = sr.Recognizer()
-    m = sr.Microphone()
-    with m as source: r.adjust_for_ambient_noise(source)      # we only need to calibrate once, before we start listening
-    self.stop_listening = r.listen_in_background(m, self.callback)
+  # def listen2(self):
+  #   r = sr.Recognizer()
+  #   m = sr.Microphone()
+  #   with m as source: r.adjust_for_ambient_noise(source)      # we only need to calibrate once, before we start listening
+  #   self.stop_listening = r.listen_in_background(m, self.callback)
+
+def activate_listner():
+  try:
+    l = Listener()
+    l.listen2()
+    while(True):
+      time.sleep(1)
+  except:
+    activate_listner()
 
 def main():
-  l = Listener()
-  l.listen2()
-  while(True):
-    time.sleep(1)
+  activate_listner()
+
+
 if __name__ == "__main__":
   main()
 
